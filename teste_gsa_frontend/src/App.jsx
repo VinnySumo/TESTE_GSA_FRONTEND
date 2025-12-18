6517c44ef0f3600e068772a00d76c6c74516e01a
VinnySumo <vinnyrafael015@hotmail.com>
2025-12-18 17:56:56 -0300

    colocado novamente a rota de detalhes do aluno

--- teste_gsa_frontend/src/App.jsx
+++ teste_gsa_frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/home';
 import Cadastro from './pages/Cadastro/cadastro';
+import Detalhes from './pages/Detalhes/detalhe';
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        {/* Rota para Edição (recebe ID e Sala) */}
-        <Route path="/cadastro/:sala/:id" element={<Cadastro />} />
+        <Route path="/cadastro/:sala/:id" element={<Cadastro />} /> {/* Rota Editar */}
+        <Route path="/detalhes/:sala/:id" element={<Detalhes />} /> {/* Rota para ver as informações do aluno */}
       </Routes>
     </BrowserRouter>
   );
